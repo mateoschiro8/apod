af474eb861076f2e5c8b2bf306b1413118f6aab6
Mateo <mateo.schiro8@gmail.com>
2024-06-01 19:16:34 -0300

    pequeños cambios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,27 @@ export default function Footer(props) {
 
     const {handleToggleModal, data} = props;
 
+    const fechaTemp = (data?.date).split(`-`);
+    const fechaImagen = [fechaTemp[1], fechaTemp[2], fechaTemp[0]].join(`/`);
+    
     return (
         <footer>
             <div className="backgroundGradient"> </div>
             <div>
-                <h1> Apod project</h1>
+                <h1> {data? fechaImagen : ""} </h1>
                 <h2> {data?.title} </h2>
             </div>
-            <button onClick={handleToggleModal}>
-                <i className="fa-solid fa-circle-info"></i>
-            </button>
-
+            <div className="buttonsBar">
+                <button onClick={handleToggleModal}>
+                    <i className="fa-solid fa-circle-info"></i>
+                </button>
+                <button onClick={handleToggleModal}>
+                    <i className="fa-solid fa-calendar-days"></i>
+                </button>
+                <button onClick={handleToggleModal}>
+                    <i className="fa-solid fa-circle-question"></i>
+                </button>
+            </div>
         </footer>
     )
 }
